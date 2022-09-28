b29c2fc11333994c503d172df4c206053312c4ed
Brixsta <Brixsta@gmail.com>
2022-09-27 18:00:15 -0600

    styling fixes

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const labels = document.querySelectorAll(".label");
 
 accordions.forEach((item, index) => {
   item.addEventListener("click", () => {
-    if (item.isClicked) {
-      item.isClicked = false;
+    if (Array.from(item.classList).includes("active")) {
       panels[index].style.height = "0px";
       labels[index].innerHTML = "+";
       labels[index].style.right = "10px";
+      item.classList.remove("active");
     } else {
       labels[index].style.right = "14px";
       labels[index].innerHTML = "-";
-      item.isClicked = true;
+      item.classList.add("active");
       panels[index].style.height = "160px";
     }
   });
